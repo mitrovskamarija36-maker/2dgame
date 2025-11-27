713b502a6876514ebc1cbaf5ddab2d6ce7190540
mitrovskamarija36-maker <mitrovskamarija36@gmail.com>
2025-11-27 14:05:01 +0100

    kraj na 5 video

---
+++ Assets/project.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class project : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
